9a6cbbc5ae9c1dffc56c0c71884a578033355837
Baptiste Doucerain <doucerain.b@gmail.com>
2019-08-12 00:00:05 +0200

    Add env config, Mongoose model & Mongoose connect

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 const router = require('express').Router()
+const MongoProfile = require('../src/model/profile')
+
 
 router.get('/', function (req, res) {
-  console.log('GET hello world')
-  res.send( 'Hello world qdqsdqsd API!')
+  res.send('Hello world from API!')
 })
 
-router.put('/profile', function (req,res) {
-  const newProfileId = 1
-  res.status(201).send(String(newProfileId))
+router.put('/profile', function (req, res) {
+  const newProfile = new MongoProfile({ first_name: req.body.first_name, last_name: req.body.last_name, gender: req.body.gender })
+  newProfile.save((err, profile) => {
+    if (err || !profile) {
+      res.sendStatus(400).send('Error : cannot add new profile')
+    } else {
+      res.status(201).send(String(profile.id))
+    }
+  })
+})
+
+router.get('/profile', async function (req, res) {
+  MongoProfile.findOne({ first_name: 'Baptiste' },
+    (err, profile) => {
+      if (err || !profile) return res.sendStatus(404)
+      return res.json(profile)
+    })
 })
 
 module.exports = router
